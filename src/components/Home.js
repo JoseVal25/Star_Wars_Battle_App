284f75bd29506bb46e5475a3913e1dcd5868cda5
Jose valencia <javhvac25@outlook.com>
2023-02-12 20:34:40 -0800

    add: componentes folder and home

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      {/* NavBar */}
+
+      {/* Cards */}
+
+      {/* List */}
+    </div>
+  );
+};
+
+export default Home;
